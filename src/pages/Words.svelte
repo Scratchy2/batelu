59454ad8fcceb645b9e75c09a00f77e9f29f5af6
Scratchy2 <152662914+Scratchy2@users.noreply.github.com>
2026-04-20 22:11:25 +0500

    4 to 5

--- src/pages/Words.svelte
+++ src/pages/Words.svelte
@@ -96,7 +96,7 @@
         (w) =>
           w.displayWord.toLowerCase().includes(normalized) ||
           (w.definition && w.definition.toLowerCase().includes(normalized)) ||
-          (DamerauLevenshteinDistance(normalized, w.displayWord.toLowerCase()) < 4) ||
+          (DamerauLevenshteinDistance(normalized, w.displayWord.toLowerCase()) < 5) ||
           (DamerauLevenshteinDistance(normalized, w.definition.toLowerCase()) < 12),
       )
     : words;
