312db3df9679673e84f935c24a53da215ae8cf8f
Scratchy2 <152662914+Scratchy2@users.noreply.github.com>
2026-04-20 23:22:12 +0500

    Change 'Grammar' link to 'Reference'

--- src/App.svelte
+++ src/App.svelte
@@ -70,7 +70,7 @@
 <nav class="nav">
 	<a href="/" on:click|preventDefault={() => navigate('/')}>Home</a>
 	<a href="/words" on:click|preventDefault={() => navigate('/words')}>Words</a>
-	<a href="/ref" on:click|preventDefault={() => navigate('/ref')}>Grammar</a>
+	<a href="/ref" on:click|preventDefault={() => navigate('/ref')}>Reference</a>
 	<a href="/phono" on:click|preventDefault={() => navigate('/phono')}>Phonology</a>
 	<a href="/inflect" on:click|preventDefault={() => navigate('/inflect')}>Inflections</a>
 	<a href="/about" on:click|preventDefault={() => navigate('/about')}>About</a>
